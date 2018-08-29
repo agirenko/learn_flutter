e40557569387b1019611822dd39af7585fbb2421
agirenko <aspnet123@gmail.com>
2018-08-29 23:29:21 +0300

    widgets tooltips

--- UdemyFlutterIntermediateCourse_Bryan_Cairns/widgets_tooltips.dart
+++ UdemyFlutterIntermediateCourse_Bryan_Cairns/widgets_tooltips.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
+  runApp(MaterialApp(
+    home: MyApp(),
   ));
 }
 
 class MyApp extends StatefulWidget {
   @override
-  _State createState() => new _State();
+  _State createState() => _State();
 }
 
 class _State extends State<MyApp> {
   String _value = 'Nothing Yet';
 
-  void _onPressed() => setState(() => _value = new DateTime.now().toString());
+  void _onPressed() => setState(() => _value = DateTime.now().toString());
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Widgets Tooltips'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widgets Tooltips'),
       ),
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
             children: <Widget>[
-              new Text(_value),
-              new IconButton(
-                icon: new Icon(Icons.timer),
+              Text(_value),
+              IconButton(
+                icon: Icon(Icons.timer),
                 onPressed: _onPressed,
                 tooltip: 'Click me',
               )
